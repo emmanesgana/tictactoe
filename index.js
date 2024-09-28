fc667c56d8077b689129178ab3ee6a4146bd45aa
emmanesgana <emman.esgana@gmail.com>
2024-09-28 12:59:23 +0800

    Added scoring system
    - Added a scoring system that increments winning player's score.

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ startButton.addEventListener("click", () => {
 
 restartButton.disabled = true;
 
-// create a module to display the game board use IIFE
 const gameBoard = (() => {
     let arr = ["", "", "", "", "", "", "", "", ""];
 
@@ -57,27 +56,40 @@ const messageController = (() => {
     }
 })();
 
-
-//create a factory for players
 const createPlayer = (name, marker) => {
     return {
         name,
-        marker
+        marker,
+        score: score = 1
     };
 }
 
-// create a module that has the games logic inside
 const game = (() => {
     let players = [];
     let currentPlayerIndex;
     let gameEnd;
+
+    const score1 = document.querySelector(".player1-score");
+    const score2 = document.querySelector(".player2-score");
 
     const start = () => {
         players = [
             createPlayer(document.querySelector("#player1").value, "X"),
             createPlayer(document.querySelector("#player2").value, "O")
         ]
+
         currentPlayerIndex = 0;
+
+        players.forEach((player, index) => {
+            if (index % 2 === 0) {
+                console.log('true')
+                score1.innerHTML = `<p class="player-name-score player-${index}" data-name="${player.name}">${player.name}: <span class="score1" data-name="${player.name}">0</span> <p>`
+            } else {
+                console.log('false')
+                score2.innerHTML = `<p class="player-name-score player-${index}" data-name="${player.name}">${player.name}: <span class="score2" data-name="${player.name}">0</span> <p>`
+            }
+        })
+
         gameEnd = false;
         gameBoard.render();
     };
@@ -93,13 +105,21 @@ const game = (() => {
 
         if (checkWinner(gameBoard.getArr(), players[currentPlayerIndex].marker)) {
             gameEnd = true;
-            messageController.renderMessage(`<span class="player-name">${players[currentPlayerIndex].name}</span> has won!`);
+            messageController.renderMessage(`<p><span class="player-name">${players[currentPlayerIndex].name}</span> has won!</p><p class="next">${players[currentPlayerIndex].name} start's next.</p>`);
+
+            //add score for each player
+            const increment = players[currentPlayerIndex].score++;
+            document.querySelector(".player-name-score").getAttribute("data-name") === players[currentPlayerIndex].name ?
+                document.querySelector(".score1").innerHTML = `<span>${increment}</span>` :
+                document.querySelector(".score2").innerHTML = `<span>${increment}</span>`;
         } else if (checkTie(gameBoard.getArr())) {
             gameEnd = true;
             messageController.renderMessage(`It's a tie!`)
+        } else {
+            currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
         }
 
-        currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+        console.log(players[currentPlayerIndex])
     };
 
     const restart = () => {
